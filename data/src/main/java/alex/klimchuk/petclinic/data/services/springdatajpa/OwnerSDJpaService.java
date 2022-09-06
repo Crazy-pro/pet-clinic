8272dfc25654bda88725fceb9ee1ff96c72e2fd8
Alex Klimchuk <Crazy.pro.noob@gmail.com>
2022-09-06 23:13:47 +0300

    Created SpringDataJPAService for Owner. Closes #36.

---
+++ data/src/main/java/alex/klimchuk/petclinic/data/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,62 @@
+package alex.klimchuk.petclinic.data.services.springdatajpa;
+
+import alex.klimchuk.petclinic.data.model.Owner;
+import alex.klimchuk.petclinic.data.repositories.*;
+import alex.klimchuk.petclinic.data.services.OwnerService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Copyright Alex Klimchuk (c) 2022.
+ */
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService {
+
+    private final OwnerRepository ownerRepository;
+    private final PetRepository petRepository;
+    private final PetTypeRepository petTypeRepository;
+
+    public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
+                             PetTypeRepository petTypeRepository) {
+        this.ownerRepository = ownerRepository;
+        this.petRepository = petRepository;
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        Set<Owner> owners = new HashSet<>();
+        ownerRepository.findAll().forEach(owners::add);
+        return owners;
+    }
+
+    @Override
+    public Owner findById(Long aLong) {
+        return ownerRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Owner findByLastName(String name) {
+        return ownerRepository.findByLastName(name);
+    }
+
+    @Override
+    public Owner save(Owner owner) {
+        return ownerRepository.save(owner);
+    }
+
+    @Override
+    public void delete(Owner owner) {
+        ownerRepository.delete(owner);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        ownerRepository.deleteById(aLong);
+    }
+
+}
