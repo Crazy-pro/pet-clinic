84fc86446465921f179907d4800a53a8a62bd557
Alex Klimchuk <Crazy.pro.noob@gmail.com>
2022-12-26 01:02:22 +0100

    Fixed problems with Css and Bootstrap

--- web/src/main/java/alex/klimchuk/petclinic/web/controllers/IndexController.java
+++ web/src/main/java/alex/klimchuk/petclinic/web/controllers/IndexController.java
@@ -1,7 +1,6 @@
 package alex.klimchuk.petclinic.web.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,7 +15,7 @@ public class IndexController {
         return new ModelAndView("/homePage");
     }
 
-    @GetMapping("/error")
+    @RequestMapping({"/error-page"})
     public ModelAndView errorPageHandler() {
         return new ModelAndView("/errorPage");
     }
