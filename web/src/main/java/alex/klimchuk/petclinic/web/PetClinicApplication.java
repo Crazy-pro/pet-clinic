0520f89fa794f08ea6a18d79a8a14caef4e97adb
Alex Klimchuk <Crazy.pro.noob@gmail.com>
2022-12-31 22:12:02 +0100

    Fixed Problem with Impl Closes #76

--- web/src/main/java/alex/klimchuk/petclinic/web/PetClinicApplication.java
+++ web/src/main/java/alex/klimchuk/petclinic/web/PetClinicApplication.java
@@ -2,12 +2,16 @@ package alex.klimchuk.petclinic.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Copyright Alex Klimchuk (c) 2022.
  */
 @SpringBootApplication
+@EntityScan(basePackages = {"alex.klimchuk.petclinic.data.model"})
+@EnableJpaRepositories(basePackages = {"alex.klimchuk.petclinic.data.repositories"})
 @ComponentScan(basePackages = {"alex.klimchuk.petclinic.web", "alex.klimchuk.petclinic.data"})
 public class PetClinicApplication {
 
